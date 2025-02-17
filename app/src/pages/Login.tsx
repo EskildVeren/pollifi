26ece86bb02f323bf678d82d3aea582b0d7906a1
EskildVeren <eskild.overen@gmail.com>
2025-02-18 00:19:31 +0100

    refactor: :recycle: add login as an imported function

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,27 +1,27 @@
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { app, firebaseAuth } from "../lib/firebase";
+import { logWithEmail } from "@/utils/login";
+import { useState } from "react";
 
-type LoginInformation = {
+export type LoginInformation = {
   username: string;
   password: string;
 };
 
 function Login() {
-  const { register, handleSubmit, watch } = useForm<LoginInformation>();
-  const onSubmit: SubmitHandler<LoginInformation> = (data) => signInWithEmailAndPassword(firebaseAuth, data.username, data.password).then((userCredential) =>{
-    const user = userCredential.user
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+  const { register, handleSubmit } = useForm<LoginInformation>();
+  const [status, setStatus] = useState("Not logged in");
+
+  const onSubmit: SubmitHandler<LoginInformation> = (data) => {
+    const user = logWithEmail(data, setStatus);
+    console.log(user);
+  };
 
   return (
-    <form className="w-full h-full flex flex-col justify-center items-center" onSubmit={handleSubmit(onSubmit)}>
-      <Card className=" w-2xs aspect-square">
+    <form className="w-full h-full flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
+      <Card className="w-2xs aspect-square">
+        <p className="text-center mt-2 mb-2">{status}</p>
         <Input placeholder="username" {...register("username")} />
         <Input placeholder="password" {...register("password")} />
         <Input type="submit" className="cursor-pointer" />
---
+++ app/src/utils/login.ts
@@ -0,0 +1,15 @@
+import { firebaseAuth } from "@/lib/firebase";
+import { LoginInformation } from "@/pages/Login";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export async function logWithEmail(data: LoginInformation, setStatus: (status: string) => void) {
+  try {
+    const userCredentials = await signInWithEmailAndPassword(firebaseAuth, data.username, data.password);
+    setStatus("Welcome " + userCredentials.user.email);
+    return userCredentials;
+  } catch (error) {
+    setStatus("Invalid credentials, please try again");
+    console.log("Errorcode: " + error.code);
+    console.log("Errormessage: " + error.message);
+  }
+}
