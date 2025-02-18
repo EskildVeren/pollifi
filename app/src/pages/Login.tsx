6cdb9ad0ca7fbf15dc3dc86d1c39fa07a58d435e
EskildVeren <eskild.overen@gmail.com>
2025-02-18 16:15:20 +0100

    style: :truck: rename username to email

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
 export type LoginInformation = {
-  username: string;
+  email: string;
   password: string;
 };
 
@@ -34,7 +34,7 @@ function Login() {
               <div className="flex flex-col gap-6">
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="m@example.com" required {...register("username")} />
+                  <Input id="email" type="email" placeholder="m@example.com" required {...register("email")} />
                 </div>
                 <div className="grid gap-2">
                   <div className="flex items-center">
--- app/src/utils/login.ts
+++ app/src/utils/login.ts
@@ -5,7 +5,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 export async function loginWithEmailAndPassword(data: LoginInformation, setStatus: (status: string) => void) {
   setStatus("Please wait...");
   try {
-    const userCredentials = await signInWithEmailAndPassword(firebaseAuth, data.username, data.password);
+    const userCredentials = await signInWithEmailAndPassword(firebaseAuth, data.email, data.password);
     setStatus("Welcome " + userCredentials.user.email);
     return userCredentials;
   } catch (error) {
