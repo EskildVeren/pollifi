cfd9e0b963907d3db91ae8b3a33d9f7dbcc35d15
Mythois <hanneswitt@gmail.com>
2025-02-22 16:12:50 +0100

    Add sign up route

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login.tsx";
+import SignUp from "./pages/SignUp.tsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/login/",
     element: <Login />,
+  },
+  {
+    path: "/signup/",
+    element: <SignUp />
   },
 ]);
 
